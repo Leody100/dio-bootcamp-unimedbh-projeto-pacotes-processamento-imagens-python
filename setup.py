afe0fd6dfb27e0b33cb9d20fa9bc57800a535ba8
Leody de Carvalho <leody100@gmail.com>
2022-10-29 21:36:29 -0300

    inicio pacote pypi

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="package_skimage",
+    version="0.0.1",
+    author="leody",
+    description="Image Processing Package using Skimage Dio_me",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="my_github_repository_project_link",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
